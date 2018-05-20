7ca855bb9b1b2408f3ff3c792961224d93d45432
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-05-20 12:02:31 +0200

    Programa 4
    escriur el que hem posat per pantalla

---
+++ Programa_4/Programa_4.ino
@@ -0,0 +1,37 @@
+/*****************************************************************
+**                                                              **
+**                        Títol:                                **
+**          Posar nom representatiu del programa                **
+**                                                              **
+** NOM:Martin Herranz Cruz                     Data:13/01/2018  **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+
+//*****************************SETUP******************************
+
+void setup() {                 //Configura el final de salida
+  
+Serial.begin(9600);
+Serial.println("Escull el numero de l'operació que vols realitzar?");
+Serial.println("  1. Comprovar numero de tarjeta de credit");
+Serial.println("  2. Comprovar numero de compte bancari");
+Serial.println("  3. Buscar un digit perdut de tarjeta de credit");
+   
+}
+
+
+
+
+
+//******************************LOOP******************************
+
+void loop() {                 //Inicia el bucle del programa
+
+
+
+}
+
+//****************************FUNCIONS****************************
